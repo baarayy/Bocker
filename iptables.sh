d66a4c2cb42d6d38ee70c185c30f233039e91ca7
baarayy <albaraa.ah26@icloud.com>
2024-05-12 09:44:53 +0300

    add iptables script

---
+++ iptables.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+DEVICE_NAME="veth"
+DEVICE_ADDR="10.88.37.11"
+
+iptables -P FORWARD DROP
+iptables -F FORWARD
+
+# Flush nat rules.
+iptables -t nat -F
+
+# Enable masquerading of ${DEVICE_ADDR}.
+iptables -t nat -A POSTROUTING -s ${DEVICE_ADDR}/24 -o eth0 -j MASQUERADE
+
+iptables -A FORWARD -i eth0 -o ${DEVICE_NAME} -j ACCEPT
+iptables -A FORWARD -o eth0 -i ${DEVICE_NAME} -j ACCEPT
